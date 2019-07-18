d6818f9ea710740a09897ef0a7ba13a1cee13687
dongkelun <dongkelun@xlpayment.com>
2019-07-18 21:15:53 +0800

    first commit

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dongkelun on 2019/7/18 19:46
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("test java");
+    }
+}
---
+++ src/main/scala/TestScala.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by dongkelun on 2019/7/16 19:52
+  */
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("Test Scala")
+  }
+}
